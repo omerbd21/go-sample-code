1b54991d9493a0c5d2f2651cbb3334464f4ce469
omer ben david <omerbendavid21@gmail.com>
2022-12-12 16:34:37 +0200

    added exercise4 ninja3

--- ninja3/exercise4.go
+++ ninja3/exercise4.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	t := time.Now()
 	birthYear := 2000
-	for t.Year() >= birthYear {
-		if t.Year() <= birthYear {
+	for {
+		if t.Year() >= birthYear {
 			fmt.Println(birthYear)
 			birthYear++
 		} else {
