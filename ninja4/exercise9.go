10e0a398f278fabc1c4e808e0322f3c071e63a36
omer ben david <omerbendavid21@gmail.com>
2022-12-12 17:52:39 +0200

    added exercise9 ninja4

---
+++ ninja4/exercise9.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		"Benda": {"Football", "DevOps"},
+		"Omer":  {"Basketball", "Kiurtush"},
+		"Sahar": {"Video Games", "Google"},
+	}
+	x["Yuvalis"] = []string{"Eating the head"}
+	for k, v := range x {
+		fmt.Println(k)
+		for i, z := range v {
+			fmt.Println(i, z)
+		}
+	}
+
+}
