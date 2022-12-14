0956cd13b177a0723f361c2a85b635b1fd2d5668
omer ben david <omerbendavid21@gmail.com>
2022-12-14 15:24:47 +0200

    added exercise1 ninja12

---
+++ ninja12/exercise1.go
@@ -0,0 +1,8 @@
+// dog is a package that deals with a dog basic functions
+package dog
+
+// Years takes an amount of human years as input and converts
+// it into dog years (adhers to the equation humanYears * 7)
+func Years(humanYears int) int {
+	return humanYears * 7
+}
