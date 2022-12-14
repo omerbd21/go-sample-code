80a23432c508a9455d31030cd407d0d3b8343b18
omer ben david <omerbendavid21@gmail.com>
2022-12-14 11:33:04 +0200

    added exercise4 ninja9

--- ninja9/exercise4.go
+++ ninja9/exercise4.go
@@ -9,13 +9,16 @@ import (
 func main() {
 	x := 0
 	var wg sync.WaitGroup
+	var mx sync.Mutex
 	wg.Add(1)
 	go func() {
+		mx.Lock()
 		x++
 		runtime.Gosched()
+		mx.Unlock()
 		fmt.Println(x)
 		wg.Done()
 	}()
-	wg.Done()
+	wg.Wait()
 	fmt.Println(x)
 }
